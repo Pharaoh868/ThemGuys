d1214c06a0c2baa760be44a84acbd840a284a3dc
aldom <aldom@ALDODL>
2023-01-06 21:34:52 -0400

    Add project files.

---
+++ ThemGuys/Guy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemGuys
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash; 
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks.");
+        }
+
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+                return 0;
+            }if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " + "I dont have enough cash to give you" + amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isnt ant amount i will take.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
